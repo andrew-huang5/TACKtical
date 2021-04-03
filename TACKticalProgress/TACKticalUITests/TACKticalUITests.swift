84e0f5de99d392cd0afa68651e3dd1f9bbe748ca
andrew-huang5 <56082986+andrew-huang5@users.noreply.github.com>
2021-04-03 12:54:37 -0700

    update

--- TACKticalProgress/TACKticalUITests/TACKticalUITests.swift
+++ TACKticalProgress/TACKticalUITests/TACKticalUITests.swift
@@ -2,33 +2,74 @@
 //  TACKticalUITests.swift
 //  TACKticalUITests
 //
-//  Created by Andrew Huang on 3/28/21.
 //
 
 import XCTest
+
+var app: XCUIApplication!
+
+extension XCUIApplication {
+    var isDisplayingHorse: Bool {
+        return otherElements["HorseView"].exists
+    }
+}
+
+extension XCUIApplication {
+    var isDisplayingRider: Bool {
+        return otherElements["RiderView"].exists
+    }
+}
+
+extension XCUIApplication {
+    var isDisplayingEdit: Bool {
+        return otherElements["EditView"].exists
+    }
+}
 
 class TACKticalUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        super.setUp()
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        
+        app.launchArguments.append("--UItesting")
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        continueAfterFailure = false
     }
 
-    func testExample() throws {
+    func testProfile() {
         // UI tests must launch the application that they test.
-        let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCTAssertTrue(app.isDisplayingHorse)
+        XCTAssertFalse(app.isDisplayingRider)
+        XCTAssertFalse(app.isDisplayingEdit)
+        
+        app.buttons["Rider Profile"].tap()
+        
+        XCTAssertTrue(app.isDisplayingRider)
+        XCTAssertFalse(app.isDisplayingHorse)
+        XCTAssertFalse(app.isDisplayingEdit)
+        
+        app.buttons["Horse Profile"].tap()
+        
+        XCTAssertTrue(app.isDisplayingHorse)
+        XCTAssertFalse(app.isDisplayingRider)
+        XCTAssertFalse(app.isDisplayingEdit)
+        
+        app.buttons["Edit Profile"].tap()
+        
+        XCTAssertTrue(app.isDisplayingEdit)
+        XCTAssertFalse(app.isDisplayingHorse)
+        XCTAssertFalse(app.isDisplayingRider)
     }
 
     func testLaunchPerformance() throws {
