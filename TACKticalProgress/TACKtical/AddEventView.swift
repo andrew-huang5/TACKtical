52b434ae9af80c94ffbae0a8acb75bec85e00e43
Haris Nashed <haris.r.nashed@vanderbilt.edu>
2021-04-17 15:03:13 -0500

    calendar

--- TACKticalProgress/TACKtical/AddEventView.swift
+++ TACKticalProgress/TACKtical/AddEventView.swift
@@ -7,32 +7,51 @@
 
 import Foundation
 import SwiftUI
+import Firebase
 
-struct CalendarView: View {
+struct AddEventView: View {
     @State var title = ""
-    @State var start = "" // Date()
-    @State var end = "" // Date()
     @State var rep = false
     @State var notify = false
+    @State var type = "Lesson"
+    
+    @State var start: Date
+    @State var end: Date
     
     var body: some View {
-        VStack {
-            TextField("Title", text: $title)
-        }.padding()
-        VStack {
-            TextField("Start", text: $start)
-        }
-        VStack {
-            TextField("End", text: $end)
-        }
-        VStack {
-            TextField("Title", text: $title)
-        }
-        Toggle(isOn: $rep) {
-            Text("Repeat")
-        }.padding()
-        Toggle(isOn: $notify) {
-            Text("Notify participants")
-        }.padding()
+        VStack{
+            HStack() {
+                Text("Title: ").foregroundColor(.blue)
+                TextField("Enter new title", text: $title)
+            }.padding(UIScreen.main.bounds.height * 0.025)
+            VStack(alignment: .leading) {
+                Text("Start:").foregroundColor(.blue)
+                DatePicker("", selection: $start, displayedComponents: [.date, .hourAndMinute])
+            }.padding(UIScreen.main.bounds.height * 0.025)
+            VStack(alignment: .leading) {
+                Text("End:").foregroundColor(.blue)
+                DatePicker("", selection: $end, displayedComponents: [.date, .hourAndMinute])
+            }.padding(UIScreen.main.bounds.height * 0.025)
+            HStack() {
+                Text("Repeat").foregroundColor(.blue)
+                Toggle(isOn: $rep) {
+                }
+            }.padding(UIScreen.main.bounds.height * 0.025)
+            HStack() {
+                Text("Notify").foregroundColor(.blue)
+                Toggle(isOn: $notify) {
+                }
+            }.padding(UIScreen.main.bounds.height * 0.025)
+            VStack() {
+                Button(action: {upload()}){
+                    Text("Done").font(.system(size:UIScreen.main.bounds.height*0.025))
+                }.frame(width:UIScreen.main.bounds.width*0.3, height:UIScreen.main.bounds.height*0.035, alignment:.center).foregroundColor(.black).background(Color(UIColor.lightGray)).opacity(0.7).cornerRadius(16).padding(UIScreen.main.bounds.height*0.005)
+            }
+        }.padding(EdgeInsets(top: 0, leading: UIScreen.main.bounds.width*0.092, bottom: 0, trailing: UIScreen.main.bounds.width*0.092)).navigationBarTitle("New Event")
+    }
+    
+    func upload() {
+        let db = Firestore.firestore()
+        db.collection("Events").document("Event3").setData(["title": title, "start": start, "end": end, "type": type, "notify": notify, "repeat": rep], merge:true)
     }
 }
--- TACKticalProgress/TACKtical/CalendarView.swift
+++ TACKticalProgress/TACKtical/CalendarView.swift
@@ -5,26 +5,133 @@
 //  Created by Haris Nashed on 4/11/21.
 //
 
-import Foundation
-import FSCalendar
 import SwiftUI
-
-//struct CalendarView: View {
-//    var calendar = FSCalendar(frame: CGRect(x: 10.0, y: 10.0, width: UIScreen.main.bounds.width * 0.7, height: UIScreen.main.bounds.height * 0.7))
-//
-//    var body: some View {
-//        calendar
-//    }
-//}
-
-struct CalendarView: UIViewRepresentable {
-
-func makeUIView(context: Context) -> UIView {
-    let calendar = FSCalendar(frame: CGRect(x: 0.0, y: 40.0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
-    calendar.scrollDirection = .vertical
-    return calendar
+import UIKit
+import FSCalendar
+    
+class CalendarData: ObservableObject{
+        
+   @Published var selectedDate : Date = Date()
+   @Published var titleOfMonth : Date = Date()
+   @Published var crntPage: Date = Date()
+        
+}
+struct CalendarView: View {
+    
+    @ObservedObject private var calendarData = CalendarData()
+    
+    var strDateSelected: String {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        dateFormatter.locale = Locale.current
+        return dateFormatter.string(from: calendarData.selectedDate)
+    }
+    
+    var strMonthTitle: String {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM yyyy"
+        dateFormatter.locale = Locale.current
+        return dateFormatter.string(from: calendarData.titleOfMonth)
+    }
+    
+    var body: some View {
+        VStack {
+            
+            HStack(spacing: 60) {
+                
+                Button(action: {
+                                    
+                    self.calendarData.crntPage = Calendar.current.date(byAdding: .month, value: -1, to: self.calendarData.crntPage)!
+                    
+                }) { Image(systemName: "arrow.left") }
+                    .frame(width: 35, height: 35, alignment: .leading)
+            
+                NavigationLink(destination: AddEventView(start: Date(), end: Date().addingTimeInterval(3600))) {
+                    Text("+").font(.system(size: 25))
+                }.frame(width: UIScreen.main.bounds.width*0.1, height:UIScreen.main.bounds.width*0.1)
+                .foregroundColor(Color.white)
+                .background(Color(red: 102/255, green: 172/255, blue: 189/255, opacity: 1.0))
+                
+                Button(action: {
+                    
+                    self.calendarData.crntPage = Calendar.current.date(byAdding: .month, value: 1, to: self.calendarData.crntPage)!
+                    
+                }) { Image(systemName: "arrow.right") }
+                .frame(width: 35, height: 35, alignment: .trailing)
+            }
+            
+            CustomCalendar(dateSelected: $calendarData.selectedDate, mnthNm: $calendarData.titleOfMonth, pageCurrent: $calendarData.crntPage)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .foregroundColor(.white)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10.0, x: 0.0, y: 0.0)
+                )
+                .frame(height: UIScreen.main.bounds.height * 0.75)
+                .padding(15)
+        }.navigationBarTitle("Schedule")
+    }
 }
 
-func updateUIView(_ uiView: UIView, context: Context) {
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
+struct CustomCalendar: UIViewRepresentable {
+   
+    typealias UIViewType = FSCalendar
+    
+    @Binding var dateSelected: Date
+    @Binding var mnthNm: Date
+    @Binding var pageCurrent: Date
+    
+    var calendar = FSCalendar()
+    
+    var today: Date{
+        return Date()
+    }
+    
+    func makeUIView(context: Context) -> FSCalendar {
+        
+        calendar.dataSource = context.coordinator
+        calendar.delegate = context.coordinator
+        calendar.appearance.headerMinimumDissolvedAlpha = 0
+       
+        return calendar
+    }
+    
+    func updateUIView(_ uiView: FSCalendar, context: Context) {
+        
+        uiView.setCurrentPage(pageCurrent, animated: true) // --->> update calendar view when click on left or right button
+    }
+    
+    func makeCoordinator() -> CustomCalendar.Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, FSCalendarDelegate, FSCalendarDataSource {
+        
+        var parent: CustomCalendar
+        
+        init(_ parent: CustomCalendar) {
+            
+            self.parent = parent
+        }
+
+        func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+            
+            parent.dateSelected = date
+        }
+        
+        func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+            
+            parent.pageCurrent = calendar.currentPage
+            parent.mnthNm = calendar.currentPage
+        }
+    }
 }
