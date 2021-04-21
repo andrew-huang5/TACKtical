8a307c53074e1f2026f55b4cd1c0f5b0297616bd
andrew-huang5 <56082986+andrew-huang5@users.noreply.github.com>
2021-04-21 11:23:37 -0700

    update

--- TACKticalProgress/TACKtical/MenuView.swift
+++ TACKticalProgress/TACKtical/MenuView.swift
@@ -9,12 +9,42 @@ import SwiftUI
 
 struct MenuView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing:80){
+            GoHome()
+            Iforgot()
+            GoCalendar()
+        }
     }
 }
 
+struct GoHome: View {
+    var body: some View{
+        NavigationLink(destination: ContentView()) {
+            Image("Home").resizable().aspectRatio(contentMode: .fill)
+        }.frame(width:UIScreen.main.bounds.width*0.06, height:UIScreen.main.bounds.height*0.06, alignment:.center)
+    }
+}
+
+struct Iforgot: View {
+    var body: some View{
+        NavigationLink(destination: ContentView()) {
+            Text("TEMP")
+            //Image("Home").resizable().aspectRatio(contentMode: .fill)
+        }.frame(width:UIScreen.main.bounds.width*0.06, height:UIScreen.main.bounds.height*0.06, alignment:.center)
+    }
+}
+
+struct GoCalendar: View {
+    var body: some View{
+        NavigationLink(destination: CalendarView()) {
+            Image("Calendar").resizable().aspectRatio(contentMode: .fill)
+        }.frame(width:UIScreen.main.bounds.width*0.06, height:UIScreen.main.bounds.height*0.06, alignment:.center)
+    }
+}
+
+/*
 struct MenuView_Previews: PreviewProvider {
     static var previews: some View {
         MenuView()
     }
-}
+}*/
