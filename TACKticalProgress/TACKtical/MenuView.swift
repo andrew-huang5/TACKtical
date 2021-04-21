87f0e60171c7d1e870b92c612a096bc9201eaadb
andrew-huang5 <56082986+andrew-huang5@users.noreply.github.com>
2021-04-21 11:21:48 -0700

    menu

---
+++ TACKticalProgress/TACKtical/MenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuView.swift
+//  TACKtical
+//
+//  Created by Andrew Huang on 4/18/21.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
